71b4fb4855a3f36e1a2b26baa72c331c4c245daf
Daria <daria.bnn@bk.ru>
2022-06-25 00:19:13 +0300

    task-3

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import Article from './components/article'
 import ColoredBlocks from './components/colored-blocks'
+import CounterInput from './components/counter-input'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
@@ -10,5 +11,6 @@ root.render(
     <App />
     <Article />
     <ColoredBlocks />
+    <CounterInput min={-10} max={30} />
   </React.StrictMode>
 )
